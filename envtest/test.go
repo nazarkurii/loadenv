b4b674071ad53f317def82235e7dfecd47324147
Nazar Kurii <nazar.nazar.kurii@gmail.com>
2026-02-15 22:01:31 +0100

    Initial Version

---
+++ envtest/test.go
@@ -0,0 +1,186 @@
+package envtest
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type Manager interface {
+	SetEnv(key, value string)
+	PathIfExists() []string
+	Cleanup()
+	New(t *testing.T)
+}
+
+type SystemEnvManager struct {
+	t       *testing.T
+	setEnvs map[string]string
+}
+
+func (sem *SystemEnvManager) SetEnv(key, value string) {
+	require.NotEmpty(sem.t, key)
+	require.NotEmpty(sem.t, value)
+
+	if sem.setEnvs == nil {
+		sem.setEnvs = make(map[string]string)
+	}
+
+	sem.setEnvs[key] = value
+
+	os.Setenv(key, value)
+}
+
+func (sem *SystemEnvManager) PathIfExists() []string {
+	return nil
+}
+
+func (sem *SystemEnvManager) Cleanup() {
+	for key := range sem.setEnvs {
+		require.NoError(sem.t, os.Unsetenv(key))
+	}
+
+	sem.setEnvs = make(map[string]string)
+}
+
+func (sem *SystemEnvManager) New(t *testing.T) {
+	t.Cleanup(sem.Cleanup)
+	sem.t = t
+}
+
+type FileEnvManager struct {
+	t        *testing.T
+	fileName string
+}
+
+func (fem *FileEnvManager) SetEnv(key, value string) {
+	fem.t.Helper()
+
+	if fem.fileName == "" {
+		file, err := os.CreateTemp("", "cfg-env-*.env")
+		fem.fileName = file.Name()
+		require.NoError(fem.t, err)
+	}
+
+	content, err := os.ReadFile(fem.fileName)
+	require.NoError(fem.t, err)
+	content = append(content, []byte("\n"+key+"="+value)...)
+
+	require.NoError(fem.t, os.WriteFile(fem.fileName, content, 0644))
+}
+
+func (fem *FileEnvManager) PathIfExists() []string {
+	fem.t.Helper()
+
+	require.NotEmpty(fem.t, fem.fileName)
+	return []string{fem.fileName}
+}
+
+func (fem *FileEnvManager) Cleanup() {
+	fem.t.Helper()
+	os.Remove(fem.fileName)
+	fem.fileName = ""
+}
+
+func (fem *FileEnvManager) New(t *testing.T) {
+	fem.t = t
+	t.Cleanup(fem.Cleanup)
+}
+
+func Test[T any](t *testing.T, loadFunc func(path ...string) (*T, error), envs Inputs) {
+
+	for _, env := range envs {
+		if err := env.validate(); err != nil {
+			t.Fatalf("provided test value error: %w", err)
+		}
+	}
+
+	t.Run("returns an error when env file does not exist", func(t *testing.T) {
+		t.Parallel()
+		cfg, err := loadFunc(uuid.New().String())
+		assert.Nil(t, cfg)
+		assert.Error(t, err)
+	})
+
+	testConfig[T](t, loadFunc, envs)
+}
+
+func testConfig[T any](t *testing.T, loadFunc func(path ...string) (*T, error), envs Inputs) {
+	t.Run("testing from a env file", func(t *testing.T) {
+		t.Parallel()
+		testEnvs[T](t, loadFunc, envs, &FileEnvManager{})
+	})
+
+	t.Run("testing from system envs", func(t *testing.T) {
+		testEnvs[T](t, loadFunc, envs, &SystemEnvManager{})
+	})
+}
+
+func testEnvs[T any](t *testing.T, loadFunc func(path ...string) (*T, error), envs Inputs, envManager Manager) {
+	for testKey, testEnv := range envs {
+		if testEnv.isRequired() {
+			t.Run("returns an error when "+testKey+" is missing", func(t *testing.T) {
+				envManager.New(t)
+
+				for key, env := range envs {
+					if key != testKey {
+						envManager.SetEnv(key, env.getValues()[0])
+					}
+				}
+
+				cfg, err := loadFunc(envManager.PathIfExists()...)
+				assert.Nil(t, cfg)
+				assert.Error(t, err)
+			})
+		}
+
+		if invalidValues := testEnv.invalidValues(); len(invalidValues) != 0 {
+			t.Run("returns an error when "+testKey+" is invalid", func(t *testing.T) {
+				envManager.New(t)
+
+				for _, invalidGetValue := range invalidValues {
+					for key, env := range envs {
+						if key != testKey {
+							envManager.SetEnv(key, env.getValues()[0])
+						} else {
+							envManager.SetEnv(key, invalidGetValue)
+						}
+					}
+
+					cfg, err := loadFunc(envManager.PathIfExists()...)
+					assert.Nil(t, cfg)
+					assert.Error(t, err)
+
+					envManager.Cleanup()
+				}
+			})
+		}
+	}
+
+	t.Run("succeeds when envs are set correctly", func(t *testing.T) {
+		envManager.New(t)
+
+		for testedKey, testedEnv := range envs {
+			values := testedEnv.getValues()
+
+			length := len(values)
+			for i := 0; i < length; i++ {
+				defer envManager.Cleanup()
+				envManager.SetEnv(testedKey, values[i])
+				for key, env := range envs {
+					if key != testedKey {
+						envManager.SetEnv(key, env.getValues()[0])
+					}
+				}
+				cfg, err := loadFunc(envManager.PathIfExists()...)
+				assert.NotNil(t, cfg)
+				assert.NoError(t, err)
+				envManager.Cleanup()
+			}
+		}
+
+	})
+}
